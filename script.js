2f75b2b4bdd914fc8cec88078fbc6610326c9655
SJ <116071092+sjarchive@users.noreply.github.com>
2026-06-01 14:58:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -174,3 +174,65 @@ function (e) {
 }
 
 );
+
+/* Ripple effect*/
+document
+.querySelectorAll(
+    ".actions button"
+)
+.forEach(button => {
+
+    button.addEventListener(
+        "click",
+        function(e){
+
+            const ripple =
+                document.createElement(
+                    "span"
+                );
+
+            ripple.classList.add(
+                "ripple"
+            );
+
+            const rect =
+                button.getBoundingClientRect();
+
+            const size =
+                Math.max(
+                    rect.width,
+                    rect.height
+                );
+
+            ripple.style.width =
+                size + "px";
+
+            ripple.style.height =
+                size + "px";
+
+            ripple.style.left =
+                e.clientX -
+                rect.left -
+                size / 2 +
+                "px";
+
+            ripple.style.top =
+                e.clientY -
+                rect.top -
+                size / 2 +
+                "px";
+
+            button.appendChild(
+                ripple
+            );
+
+            setTimeout(() => {
+
+                ripple.remove();
+
+            }, 600);
+
+        }
+    );
+
+});
